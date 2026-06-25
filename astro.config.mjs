ca700879340e249a76626d2b9627a9f0c9efee1a
Herob527 <szymon.wrzos@onet.pl>
2026-06-25 14:48:37 +0200

    Add preset attributify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 import preact from '@astrojs/preact'
+import presetAttributify from '@unocss/preset-attributify'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     preact(),
     UnoCSS({
       injectReset: true,
+      presets: [presetAttributify()],
     }),
   ],
 })
