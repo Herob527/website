e432557fd70c4c2380afcb3550a99247d46ec5d6
Herob527 <szymon.wrzos@onet.pl>
2026-06-25 23:11:54 +0200

    Add attributyify transformer

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'unocss'
-
-import presetAttributify, {
-  extractorAttributify,
-} from '@unocss/preset-attributify'
+import { defineConfig, transformerAttributifyJsx } from 'unocss'
+import presetAttributify from '@unocss/preset-attributify'
 import { presetIcons, presetWind4 } from 'unocss'
+
 export default defineConfig({
   presets: [presetAttributify(), presetWind4(), presetIcons()],
-  extractors: [extractorAttributify()],
+  transformers: [transformerAttributifyJsx()],
 })
