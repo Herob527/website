95eaf50f132550c9f00573325be641335373b989
Herob527 <szymon.wrzos@onet.pl>
2026-06-25 14:32:06 +0200

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), UnoCSS({
+    injectReset: true
+  })]
+});
