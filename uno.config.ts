6ed527d63257877649e3e064b096ae72b655f166
Herob527 <szymon.wrzos@onet.pl>
2026-06-26 09:00:41 +0200

    Remove jsx

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, transformerAttributifyJsx } from 'unocss'
+import { defineConfig } from 'unocss'
 import presetAttributify from '@unocss/preset-attributify'
 import { presetIcons, presetWind4 } from 'unocss'
 
 export default defineConfig({
   presets: [presetAttributify(), presetWind4(), presetIcons()],
-  transformers: [transformerAttributifyJsx()],
 })
