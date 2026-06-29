815253cd0b830b62f17013f8e80dd1c8bc025e9d
Herob527 <szymon.wrzos@onet.pl>
2026-06-29 10:31:19 +0200

    Adde typography

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetTypography } from 'unocss'
 import presetAttributify from '@unocss/preset-attributify'
 import { presetIcons, presetWind4 } from 'unocss'
 
 export default defineConfig({
-  presets: [presetAttributify(), presetWind4(), presetIcons()],
+  presets: [
+    presetAttributify(),
+    presetWind4(),
+    presetIcons(),
+    presetTypography(),
+  ],
 })
