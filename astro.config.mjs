714d7c4b79574760b0f61c0a61d501488dec10da
Herob527 <szymon.wrzos@onet.pl>
2026-07-01 19:14:06 +0200

    Add site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://herob527.github.io',
+  base: 'website',
   experimental: {
     contentIntellisense: true,
     svgOptimizer: svgoOptimizer(),
