7c15f72382ab407dd7576f3aebffbce0e6f631d5
Herob527 <szymon.wrzos@onet.pl>
2026-07-04 08:33:08 +0200

    Make base root in dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import yaml from '@rollup/plugin-yaml'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://herob527.github.io',
-  base: '/website',
+  base: import.meta.env.DEV ? '/' : '/website',
   experimental: {
     contentIntellisense: true,
     svgOptimizer: svgoOptimizer(),
