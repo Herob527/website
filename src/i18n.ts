880894a4b2d7abc73e1ed32caa346e8703da4f36
Herob527 <szymon.wrzos@onet.pl>
2026-07-05 16:09:19 +0200

    Working out multilanguage

---
+++ src/i18n.ts
@@ -0,0 +1,42 @@
+import type { GetStaticPathsItem } from 'astro'
+import i18next from 'i18next'
+
+const translations = import.meta.glob('./locales/*.yaml', {
+  eager: true,
+  import: 'default',
+})
+
+const transformed = Object.fromEntries(
+  Object.entries(translations).map(([key, val]) => {
+    const locale = /\/(.{2,4})\.yaml/.exec(key)?.at(1)
+    if (!locale) throw new Error('Invalid locale')
+    return [locale, { translation: val }]
+  }),
+) as Record<string, Record<string, string>>
+
+export const locales = Object.keys(transformed)
+
+export const initI18n = async () =>
+  i18next.init({
+    lng: 'pl',
+    resources: transformed,
+  })
+
+export const geti18nStaticPaths = () => {
+  const items = locales.map(
+    (el) =>
+      ({
+        params: {
+          locale: el,
+        },
+      }) as const,
+  )
+  return [
+    ...items,
+    {
+      params: {
+        locale: undefined,
+      } as const,
+    },
+  ] satisfies GetStaticPathsItem[]
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,8 @@
 import { defineMiddleware } from 'astro:middleware'
 import i18next from 'i18next'
+import { initI18n } from './i18n'
 
-const translations = import.meta.glob('./locales/*.yaml', {
-  eager: true,
-  import: 'default',
-})
-
-const transformed = Object.fromEntries(
-  Object.entries(translations).map(([key, val]) => {
-    const locale = /\/(.{2,4})\.yaml/.exec(key)?.at(1)
-    if (!locale) throw new Error('Invalid locale')
-    return [locale, { translation: val }]
-  }),
-) as Record<string, Record<string, string>>
-
-await i18next.init({
-  lng: 'pl',
-  resources: transformed,
-})
+await initI18n()
 
 const onRequest = defineMiddleware(async (context, next) => {
   const { currentLocale } = context
