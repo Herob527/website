033dea900de0554405bacadea239b0f6be001833
Herob527 <szymon.wrzos@onet.pl>
2026-07-06 11:41:03 +0200

    Testing out

--- scripts/redact-blog.ts
+++ scripts/redact-blog.ts
@@ -4,16 +4,30 @@ import type { Model } from './models'
 
 const client = new LMStudioClient()
 
-const modelName: Model = 'microsoft/phi-4-reasoning-plus'
+const models: Model[] = [
+  'microsoft/phi-4-reasoning-plus',
+  'zai-org/glm-4.7-flash',
+  'google/gemma-4-26b-a4b-qat',
+  'openai/gpt-oss-20b',
+]
 
-const model = await client.llm.model(modelName)
+for (const modelName of models) {
+  const model = await client.llm.model(modelName, {
+    config: {
+      contextLength: 512,
+    },
+  })
 
-const result = await model.respond("Testing. Respond with 'yes' only.", {
-  maxTokens: 10,
-})
+  const result = await model.respond(
+    "Testing. Respond with 'yes' only without thinking.",
+    {
+      maxTokens: 10,
+    },
+  )
 
-await model.unload()
+  await model.unload()
 
-console.info(result.content)
+  console.info({ modelName, content: result.content, info: result.modelInfo })
+}
 
 exit(0)
