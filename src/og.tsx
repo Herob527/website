c60cf6998a39bc9ed639a978ee3d2576f58196ca
Herob527 <szymon.wrzos@onet.pl>
2026-07-12 16:30:22 +0200

    Working on markup

--- src/og.tsx
+++ src/og.tsx
@@ -1,9 +1,18 @@
 import { fromJsx } from 'takumi-js/helpers/jsx'
 import type { BlogPost } from './pages/[...locale]/blog/[slug]/og.png'
+import { dateUtils } from './date-utils'
 
-export const ogMarkup = async (post: BlogPost) => {
+export const ogMarkup = async (post: BlogPost, locale: string) => {
+  const date = dateUtils.parse(post.date)
   const data = await fromJsx(
-    <div tw="bg-blue-500 text-3xl text-white p-4">{post.title}</div>,
+    <div tw="flex flex-col">
+      <div tw="bg-blue-500 text-3xl text-white p-4">{post.title}</div>,
+      <div tw="bg-blue-500 text-3xl text-white p-4">{post.description}</div>,
+      <div tw="bg-blue-500 text-3xl text-white p-4">
+        {dateUtils.format(date, locale)}
+      </div>
+      ,
+    </div>,
   )
   return data
 }
