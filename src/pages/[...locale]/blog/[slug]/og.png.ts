c3c69d2c34de8536691683e0520a73a45e1bfb1b
Herob527 <szymon.wrzos@onet.pl>
2026-07-12 14:22:52 +0200

    Added static paths

--- src/pages/[...locale]/blog/[slug]/og.png.ts
+++ src/pages/[...locale]/blog/[slug]/og.png.ts
@@ -2,6 +2,9 @@ import { ogMarkup } from '@root/src/og'
 import type { APIRoute } from 'astro'
 import ImageResponse, { type ImageResponseOptions } from 'takumi-js/response'
 
+import { locales } from '@root/src/i18n'
+import { getCollection } from 'astro:content'
+
 const OUTPUT_TYPE: ImageResponseOptions['format'] = 'png'
 
 const DIMENSIONS = {
@@ -9,7 +12,8 @@ const DIMENSIONS = {
   height: 630,
 }
 
-export const GET: APIRoute = async ({ params }) => {
+export const GET: APIRoute = async ({ params, props }) => {
+  const { post } = props as Props
   const toRender = await ogMarkup()
 
   const image = await new ImageResponse(toRender.node, {
@@ -24,4 +28,46 @@ export const GET: APIRoute = async ({ params }) => {
       'Cache-Control': 'public, max-age=31536000, immutable',
     },
   })
+}
+
+export async function getStaticPaths() {
+  const collections = (
+    await Promise.all(
+      [...locales, undefined].flatMap(async (locale) => {
+        const data = await getCollection('blog')
+        return data
+          .filter((el) => {
+            const postLocale = el.id.split('/')[0]
+            return locales.includes(postLocale)
+          })
+          .map((el) => {
+            const parts = el.id.split('/')
+            const slug = parts.length > 1 ? parts.slice(1).join('/') : el.id
+            const postLocale = parts[0]
+            return { slug, locale: locale, postLocale, postData: el.data }
+          })
+      }),
+    )
+  ).reduce((acc, cur) => [...acc, ...cur].flat(), [])
+
+  return collections
+    .filter(
+      (entry) =>
+        entry.locale === undefined || entry.locale === entry.postLocale,
+    )
+    .map((entry) => ({
+      params: {
+        slug: entry.slug,
+        locale: entry.locale,
+      },
+      props: {
+        post: entry.postData,
+      },
+    }))
+}
+
+type BlogPost = Awaited<ReturnType<typeof getCollection<'blog'>>>[number]
+
+interface Props {
+  post: BlogPost['data']
 }
